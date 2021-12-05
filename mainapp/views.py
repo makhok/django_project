4f5bda6c43bfbc18e85a98b6403fb5664a847cb7
makhok <makhok@yandex.ru>
2021-12-05 20:41:29 +0300

    Домашнее задание №2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,38 @@
+import json
+import os.path
+
 from django.shortcuts import render
 
 # Create your views here.
 
-def main(request):
-    return render(request, 'mainapp/index.html')
+main_menu = [
+    {'href': 'index', 'name': 'домой'},
+    {'href': 'products', 'name': 'продукты'},
+    {'href': 'contact', 'name': 'контакты'},
+]
+
+module_dir = os.path.dirname(__file__)
+
+def index(request):
+    content = {
+        'title': 'магазин',
+        'main_menu': main_menu,
+    }
+    return render(request, 'mainapp/index.html', content)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    file_path = os.path.join(module_dir, 'fixtures/products.json')
+    products = json.load(open(file_path, encoding='utf-8'))
+    content = {
+        'title': 'каталог',
+        'main_menu': main_menu,
+        'products': products,
+    }
+    return render(request, 'mainapp/products.html', content)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'контакты',
+        'main_menu': main_menu,
+    }
+    return render(request, 'mainapp/contact.html', content)
